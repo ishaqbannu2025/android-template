382c619653f281a9c45f7b539b8916d1c944ffe0
ishaqbannu2025 <m.ishaqbannu@gmail.com>
2025-01-26 17:18:57 +0500

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.webviewapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("{{URL}}")  // Placeholder for the URL
+    }
+}
